0cca4d52f20d1ad676470934e8bb7d28a4167a2f
YangHang <18301205109@163.com>
2019-08-11 21:41:35 +0800

    2019/08/11 项目提交--新建注册中心客户端工程

--- eureka-server/src/main/java/com/yanghang/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/yanghang/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,11 @@ package com.yanghang.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// EnableEurekaServer注解声明这是一个eureka注册中心的服务端
+
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
